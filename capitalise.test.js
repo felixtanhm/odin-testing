c8134bb215e3baeb3fc84bb75a09824314e7e4a1
felixtanhm <felixtanhm@gmail.com>
2023-12-17 16:54:52 +0900

    Improvement | Added more tests for capitalise function.

--- capitalise.test.js
+++ capitalise.test.js
@@ -1,5 +1,33 @@
 import capitalise from "./capitalise";
 
-test("'testing' is returned as 'Testing'", () => {
+test('"testing" should return "Testing"', () => {
   expect(capitalise("testing")).toBe("Testing");
+});
+
+test('"Testing" should return "Testing"', () => {
+  expect(capitalise("Testing")).toBe("Testing");
+});
+
+test('"TESTING" should return "TESTING"', () => {
+  expect(capitalise("TESTING")).toBe("TESTING");
+});
+
+test('"this is a test" should return "This is a test"', () => {
+  expect(capitalise("this is a test")).toBe("This is a test");
+});
+
+test('"12@#abcd@#34" should return "12@#Abcd@#34"', () => {
+  expect(capitalise("12@#abcd@#34")).toBe("12@#Abcd@#34");
+});
+
+test('"1234" should return "No valid characters."', () => {
+  expect(capitalise("1234")).toBe("No valid characters.");
+});
+
+test('"@#$" should return "No valid characters."', () => {
+  expect(capitalise("@#$")).toBe("No valid characters.");
+});
+
+test('1234 should return "Not a string."', () => {
+  expect(capitalise(1234)).toBe("Not a string.");
 });
