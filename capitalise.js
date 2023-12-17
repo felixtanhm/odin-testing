47dbed55b64df15dfe1fb660563a0ebaa6e6da0e
felixtanhm <felixtanhm@gmail.com>
2023-12-17 17:12:01 +0900

    Improvement | Capitalise returns not string error.

--- capitalise.js
+++ capitalise.js
@@ -2,7 +2,7 @@ function capitalise(string) {
   if (typeof string == "string") {
     return string[0].toUpperCase() + string.slice(1);
   }
-  return "Not a string";
+  return "Not a string.";
 }
 
 export default capitalise;
