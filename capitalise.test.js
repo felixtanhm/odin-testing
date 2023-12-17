76cbaed9ac8678a2a3f229c25d499c74a8738c14
felixtanhm <felixtanhm@gmail.com>
2023-12-17 17:11:17 +0900

    Improvement | Removed tests for testing non-letters, requires Regex.

--- capitalise.test.js
+++ capitalise.test.js
@@ -16,18 +16,6 @@ test('"this is a test" should return "This is a test"', () => {
   expect(capitalise("this is a test")).toBe("This is a test");
 });
 
-test('"12@#abcd@#34" should return "12@#Abcd@#34"', () => {
-  expect(capitalise("12@#abcd@#34")).toBe("12@#Abcd@#34");
-});
-
-test('"1234" should return "No valid characters."', () => {
-  expect(capitalise("1234")).toBe("No valid characters.");
-});
-
-test('"@#$" should return "No valid characters."', () => {
-  expect(capitalise("@#$")).toBe("No valid characters.");
-});
-
 test('1234 should return "Not a string."', () => {
   expect(capitalise(1234)).toBe("Not a string.");
 });
