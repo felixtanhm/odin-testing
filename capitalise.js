2d16c04fd4b511c94351c64dc121b1afe27720c5
felixtanhm <felixtanhm@gmail.com>
2023-12-19 23:40:37 +0900

    Improvement | Delete unnecessary files.

--- capitalise.js
+++
@@ -1,8 +0,0 @@
-function capitalise(string) {
-  if (typeof string == "string") {
-    return string[0].toUpperCase() + string.slice(1);
-  }
-  return "Not a string.";
-}
-
-export default capitalise;
--- sum.js
+++
@@ -1,8 +0,0 @@
-function sum(a, b) {
-  if (typeof a == "number" && typeof b == "number") {
-    return a + b;
-  }
-  return "Inputs need to be integers.";
-}
-
-export default sum;
