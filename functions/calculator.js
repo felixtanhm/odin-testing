ef57876e5854650ae1dd523360dff40eaf99b880
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-20 14:02:25 +0100

    extend function for validating number parameter

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,22 +1,26 @@
 const calculator = (() => {
   function sum(a, b) {
-    return numCheck(a, b) ? a + b : "Inputs contain a non-number.";
+    return numCheck(a, b) ? a + b : 'Inputs contain a non-number.';
   }
 
   function subtract(a, b) {
-    return numCheck(a, b) ? a - b : "Inputs contain a non-number.";
+    return numCheck(a, b) ? a - b : 'Inputs contain a non-number.';
   }
 
   function multiply(a, b) {
-    return numCheck(a, b) ? a * b : "Inputs contain a non-number.";
+    return numCheck(a, b) ? a * b : 'Inputs contain a non-number.';
   }
 
   function divide(a, b) {
-    return numCheck(a, b) ? a / b : "Inputs contain a non-number.";
+    return numCheck(a, b) ? a / b : 'Inputs contain a non-number.';
   }
 
   function numCheck(a, b) {
-    return typeof a == "number" && typeof b == "number";
+    return isValidNumber(a) && isValidNumber(b);
+  }
+
+  function isValidNumber(number) {
+    return !Number.isNaN(number) && typeof number === 'number';
   }
   return { sum, multiply, subtract, divide };
 })();
