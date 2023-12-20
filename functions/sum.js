068ff9473352acbec1cdd564d5f2a88ec73172fb
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-20 13:54:08 +0100

    extend with a helper function to validate parameter

--- functions/sum.js
+++ functions/sum.js
@@ -1,4 +1,12 @@
+function isValidNumber(number) {
+  return (!Number.isNaN(number) && typeof number === "number");
+}
+
 function sum(a, b) {
+  if (!isValidNumber(a) || !isValidNumber(b)) {
+    return "Inputs need to be integers.";
+  }
+
   if (typeof a == "number" && typeof b == "number") {
     return a + b;
   }
